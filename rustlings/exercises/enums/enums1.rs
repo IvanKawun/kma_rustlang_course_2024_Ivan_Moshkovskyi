6d21be6d4502f544b2023267ea5a3adf609bf0ef
123 <van.moshk@gmail.com>
2024-11-14 08:46:05 +0300

    Third commit

--- rustlings/exercises/enums/enums1.rs
+++ rustlings/exercises/enums/enums1.rs
@@ -1,16 +1,20 @@
 // enums1.rs
 // Make me compile! Execute `rustlings hint enums1` for hints!
 
-// I AM NOT DONE
+// I AM DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(i32, i32, i32)
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo("Hello, world!".to_string()));
+    println!("{:?}", Message::Move { x: 10, y: 20 });
+    println!("{:?}", Message::ChangeColor(255, 0, 0));
 }
--- rustlings/exercises/option/option3.rs
+++ rustlings/exercises/option/option3.rs
@@ -1,7 +1,7 @@
 // option3.rs
 // Make me compile! Execute `rustlings hint option3` for hints
 
-// I AM NOT DONE
+// I AM DONE
 
 struct Point {
     x: i32,
@@ -15,5 +15,5 @@ fn main() {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
-    y; // Fix without deleting this line.
+    let y = Some(Point { x: 100, y: 200 }); // Fix without deleting this line.
 }
